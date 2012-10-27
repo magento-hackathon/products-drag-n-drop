268aa3a9356d638721c9546da6781a860cc0081f
Michael Türk <michael.tuerk@flagbit.de>
2012-10-27 20:42:27 +0200

    [+FEATURE] Added sorting functionality and frontend controller

---
+++ src/app/code/community/Hackathon/ProductDnD/controllers/Frontend/SortController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: flagbit
+ * Date: 27.10.12
+ * Time: 16:38
+ * To change this template use File | Settings | File Templates.
+ */
+class Hackathon_ProductDnD_Frontend_SortController extends Mage_Core_Controller_Front_Action
+{
+    public function changeProductPositionAction()
+    {
+        if (!Mage::helper('hackathon_productdnd')->isActivated()) {
+            return false;
+        }
+
+        $request = $this->getRequest();
+        $categoryId = (int) $request->getParam('category_id');
+        $productId = (int) $request->getParam('product_id');
+        $neighborId = (int) $request->getParam('neighbor_id');
+
+        Mage::getModel('hackathon_productdnd/sorter')->changeProductPosition($categoryId, $productId, $neighborId);
+    }
+}
