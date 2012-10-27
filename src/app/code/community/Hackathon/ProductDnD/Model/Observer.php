fdfa75eeacecc1abbd2dc5c65da73d59ebce5380
Marco Becker <marco.becker@flagbit.de>
2012-10-27 16:16:28 +0200

    [TASK] append sortable.js template to the response after the ajax load of the category products

--- src/app/code/community/Hackathon/ProductDnD/Model/Observer.php
+++ src/app/code/community/Hackathon/ProductDnD/Model/Observer.php
@@ -6,7 +6,7 @@
  * Time: 13:54
  * To change this template use File | Settings | File Templates.
  */
-class Hackathon_ProductDnD_Model_Observer
+class Hackathon_ProductDnD_Model_Observer extends Mage_Core_Block_Template
 {
     /**
      * Adds layout handles based on configuration.
@@ -25,4 +25,20 @@ class Hackathon_ProductDnD_Model_Observer
             $update->addHandle('hackathon_productdnd_enabled');
         }
     }
+	
+	/**
+	 * Adds the sortable.js to the head of the page after the ajax-Request for the category-products
+	 * was loaded
+	 * 
+	 * @param Varien_Event_Observer $observer
+	 */
+	 public function addSortableScript($observer)
+	 {
+	 	$this->setTemplate('hackathon/productdnd/sortable.phtml');
+		// Mage::log(get_class_methods($this));
+		$content = $observer->getResponse()->getContent();
+		$additional = $this->toHtml();
+		Mage::log($additional);
+		$content .= $additional;
+	 }
 }
