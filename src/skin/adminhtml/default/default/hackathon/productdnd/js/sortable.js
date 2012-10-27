9d4d001e996d87b39faf68001724f4985724bdaa
Tim Bezhashvyly <tim@openstream.ch>
2012-10-27 14:18:05 +0200

    sortable.js logic

--- src/skin/adminhtml/default/default/hackathon/productdnd/js/sortable.js
+++ src/skin/adminhtml/default/default/hackathon/productdnd/js/sortable.js
@@ -1,1 +1,5 @@
-alert('works!');
+document.observe("dom:loaded", function()
+{
+    $('catalog_category_products_table').down('tbody').setAttribute('id', 'catalog_category_products_table_tbody');
+    Sortable.create("catalog_category_products_table_tbody", { tag: "tr" });
+});
