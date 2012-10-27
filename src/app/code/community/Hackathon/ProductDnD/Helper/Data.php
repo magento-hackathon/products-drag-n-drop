c43fa06bd57af780f2aa1d0575909a431c2f9662
Michael Türk <michael.tuerk@flagbit.de>
2012-10-27 13:58:18 +0200

    [+FEATURE] Added helper for check for activation

---
+++ src/app/code/community/Hackathon/ProductDnD/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: flagbit
+ * Date: 27.10.12
+ * Time: 13:57
+ * To change this template use File | Settings | File Templates.
+ */
+class Hackathon_ProductDnD_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+    public function isAllowed()
+    {
+        return true;
+    }
+
+}
