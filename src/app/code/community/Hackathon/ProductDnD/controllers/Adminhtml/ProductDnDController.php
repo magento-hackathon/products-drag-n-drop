a724f701f9b39875a72960581c229384177ddac8
thakilla <essig@flagbit.de>
2012-10-27 19:16:20 +0200

    [+TASK] finished backend javascript

--- src/app/code/community/Hackathon/ProductDnD/controllers/Adminhtml/ProductDnDController.php
+++ src/app/code/community/Hackathon/ProductDnD/controllers/Adminhtml/ProductDnDController.php
@@ -3,7 +3,7 @@
 class Hackathon_ProductDnD_Adminhtml_ProductDnDController extends Mage_Adminhtml_Controller_Action
 {
     public function ajaxBlockAction(){
-        $this->getResponse()->setBody('ajax action controller');
+        //$this->getResponse()->setBody(Mage::app()->getRequest()->getParam('delta'));
         return;
     }
 }
