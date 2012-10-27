b3c4711b0f21a306fe9bba9dc283a2da550caf16
Michael Türk <michael.tuerk@flagbit.de>
2012-10-27 16:17:25 +0200

    [+FEATURE] Added first release of configuration and fe validation

--- src/app/code/community/Hackathon/ProductDnD/Helper/Data.php
+++ src/app/code/community/Hackathon/ProductDnD/Helper/Data.php
@@ -8,10 +8,41 @@
  */
 class Hackathon_ProductDnD_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const XML_PATH_DRAGNDROP_ALLOW_IPS = 'catalog/dragndrop/allow_ips';
+    const XML_PATH_DRAGNDROP_ACTIVE_BACKEND = 'catalog/dragndrop/active_backend';
+    const XML_PATH_DRAGNDROP_ACTIVE_FRONTEND = 'catalog/dragndrop/active_frontend';
 
-    public function isActivated()
+    /**
+     * @param Mage_Core_Model_Store $store
+     * @return bool
+     */
+    public function isActivated($store = null)
     {
-        return true;
+
+        if (Mage::getDesign()->getArea() == 'adminhtml') {
+            return self::XML_PATH_DRAGNDROP_ACTIVE_BACKEND;
+        }
+
+        if (is_null($store)) {
+            $store = Mage::app()->getStore();
+        }
+        if (!$store instanceof Mage_Core_Model_Store) {
+            $store = Mage::app()->getStore($store);
+        }
+
+        $allow = Mage::getStoreConfigFlag(self::XML_PATH_DRAGNDROP_ACTIVE_FRONTEND, $store);
+
+        $allowedIps = Mage::getStoreConfig(self::XML_PATH_DRAGNDROP_ALLOW_IPS, $store->getId());
+        $remoteAddr = Mage::helper('core/http')->getRemoteAddr();
+        if ($allow && !empty($allowedIps) && !empty($remoteAddr)) {
+            $allowedIps = preg_split('#\s*,\s*#', $allowedIps, null, PREG_SPLIT_NO_EMPTY);
+            if (array_search($remoteAddr, $allowedIps) === false
+                && array_search(Mage::helper('core/http')->getHttpHost(), $allowedIps) === false) {
+                $allow = false;
+            }
+        }
+
+        return $allow;
     }
 
 }
