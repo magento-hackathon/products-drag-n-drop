39a6c484aa76f1eb233fed868a79072d6ec0fd2f
Mike Becker <mike@brainstation.de>
2012-10-28 13:34:41 +0100

    [+FEATURE] Change sorting algorithm to support same positions of 2 products

--- src/app/code/community/Hackathon/ProductDnD/Model/Sorter.php
+++ src/app/code/community/Hackathon/ProductDnD/Model/Sorter.php
@@ -14,9 +14,10 @@ class Hackathon_ProductDnD_Model_Sorter extends Mage_Core_Model_Abstract
         $category = Mage::getModel('catalog/category')->load($categoryId);
 
         /* @var $category Mage_Catalog_Model_Category */
-        $positions = $oldPositions = $category->getProductsPosition();
-        $flippedPositions = array_flip($positions);
-        if (!in_array($productId, $flippedPositions) || !in_array($neighborId, $flippedPositions)) {
+        $oldPositions = $category->getProductsPosition();
+        $positions = array_keys($oldPositions);
+
+        if (!in_array($productId, $positions) || !in_array($neighborId, $positions)) {
             return array(
                 'categoryId' => $categoryId,
                 'productId' => $productId,
@@ -26,48 +27,20 @@ class Hackathon_ProductDnD_Model_Sorter extends Mage_Core_Model_Abstract
             return false;
         }
 
-        ksort($flippedPositions);
-        $positionKeys = array_keys($flippedPositions);
+        array_multisort($oldPositions, $positions);
+        $posProductId = array_search($productId, $positions);
+        $posNeighborId = array_search($neighborId, $positions);
+        unset($positions[$posProductId]);
 
-        $originalProductPosition = $positions[$productId];
-        $originalNeighborPosition = $positions[$neighborId];
-
-        if ($originalProductPosition < $originalNeighborPosition) {
-            $positionKeys = array_reverse($positionKeys);
-
-            foreach ($positionKeys as $key => $value) {
-                if ($value > $originalNeighborPosition) {
-                    continue;
-                }
-                if ($value <= $originalProductPosition) {
-                    break;
-                }
-
-                $positions[$flippedPositions[$value]] = $positions[$flippedPositions[$positionKeys[$key + 1]]];
-            }
-        }
-        else {
-            foreach ($positionKeys as $key => $value) {
-                if ($value < $originalNeighborPosition) {
-                    continue;
-                }
-                if ($value >= $originalProductPosition) {
-                    break;
-                }
-
-                $positions[$flippedPositions[$value]] = $positions[$flippedPositions[$positionKeys[$key + 1]]];
-            }
-        }
-
-        $positions[$productId] = $originalNeighborPosition;
+        $firstPart = array_slice($positions, 0, $posNeighborId);
+        array_push($firstPart, $productId);
+        $positions = array_flip(array_merge($firstPart, array_slice($positions, $posNeighborId)));
 
         if ($oldPositions != $positions) {
             $category->setPostedProducts($positions);
             $category->save();
         }
 
-        $flippedPositions = array_flip($positions);
-        ksort($flippedPositions);
-        return $flippedPositions;
+        return $positions;
     }
 }
